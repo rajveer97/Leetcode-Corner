2ad8d1f5a39e8266bd649efc0b593f3fcc58de34
Rajveer Pandey <66234228+rajveer97@users.noreply.github.com>
2022-08-06 23:39:51 +0530

    Add files via upload

---
+++ Problem of the Day/458.PoorPigs.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        int t = minutesToTest/minutesToDie;
+        
+        return ceil(log(buckets) / log(t+1));
+    }
+};
