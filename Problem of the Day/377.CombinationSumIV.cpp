906a450e8f651cf1ff3954109312924030ae5afc
Rajveer Pandey <66234228+rajveer97@users.noreply.github.com>
2022-08-08 11:15:24 +0530

    Update 377.CombinationSumIV.cpp

--- Problem of the Day/377.CombinationSumIV.cpp
+++ Problem of the Day/377.CombinationSumIV.cpp
@@ -1,22 +1,81 @@
+ // TLE
 class Solution {
 public:
-    
-    vector<int> dp;
-    
-    Solution() {
-        dp.resize(1001);
-        fill(dp.begin(), dp.end(), -1);
+    int solve(vector<int>&nums, int target)
+    {
+        if(target < 0) return 0;
+        if(target == 0) return 1;
+        
+        int ans = 0;
+        for(int i=0; i<nums.size(); i++)
+        {
+            ans += solve(nums, target-nums[i]);
+        }
+        
+        return ans;
     }
     
-    int combinationSum4(vector<int>& nums, int target, int currSum=0) {
-        if(currSum > target) return 0;
-        if(currSum == target) return 1;
-        if(dp[currSum] != -1) return dp[currSum];
-        int ways = 0;
-        for(int i=0; i<nums.size(); i++) {
-            if(currSum + nums[i] <= target)
-                ways += combinationSum4(nums, target, currSum + nums[i]);
+    int combinationSum4(vector<int>& nums, int target) {
+        return solve(nums, target);
+    }
+};
+
+
+//-------------------------------------------------------
+
+// Method 2: Memoization
+class Solution {
+public:
+    int solve(vector<int>&nums, int target, vector<int> &dp)
+    {
+        if(target < 0) return 0;
+        if(target == 0) return 1;
+        
+        if(dp[target] != -1)
+            return dp[target];
+        
+        int ans = 0;
+        for(int i=0; i<nums.size(); i++)
+        {
+            ans += solve(nums, target-nums[i], dp);
         }
-        return dp[currSum] = ways;
+        
+        dp[target] = ans;
+        return dp[target];
+    }
+    
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> dp(target+1, -1);
+        return solve(nums, target, dp);
+    }
+};
+
+
+//-----------------------------------------------------------
+// Method 3: Tabulation DP
+class Solution {
+public:
+    int solve(vector<int>&nums, int target)
+    {
+        vector<long long int> dp(target+1, 0);
+        
+        dp[0] = 1;
+        
+        // Traversing tar from 1 to target
+        for(int i = 1; i <= target; i++)
+        {
+            // Traversing on the nums vector
+            for(int j=0; j<nums.size(); j++)
+            {
+                if(i - nums[j] >= 0)
+                    dp[i] = dp[i] + (int)dp[i - nums[j]];
+            }
+        }
+        
+        return dp[target];
+    }
+    
+    int combinationSum4(vector<int>& nums, int target) {
+        return solve(nums, target);
     }
 };
