28c03fb0935f6007dedc18fb0d82174f5ccd4d0a
Rajveer Pandey <kram35477@gmail.com>
2022-09-02 14:39:14 +0530

    Date: 02.09.2022

---
+++ Problem of the Day/637.AverageOfLevelsInBinaryTree.cpp
@@ -0,0 +1,41 @@
+
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        vector<double> ans;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while(!q.empty())
+        {
+            int sz = q.size();
+            double sum = 0.0;
+            for(int i=0; i<sz; i++)
+            {
+                TreeNode* curr = q.front();
+                q.pop();
+                sum += curr->val;
+                
+                if(curr->left) q.push(curr->left);
+                if(curr->right) q.push(curr->right);
+            }
+            
+            ans.push_back(sum/sz);
+        }
+        
+        return ans;
+    }
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
