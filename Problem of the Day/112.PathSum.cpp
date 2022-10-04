5de86d83b75ee03647dce347ebe3641beef54fb7
Rajveer Pandey <kram35477@gmail.com>
2022-10-04 12:21:18 +0530

    Date: 05.10.22

--- Problem of the Day/112.PathSum.cpp
+++ Problem of the Day/112.PathSum.cpp
@@ -1,14 +1,36 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// Method 1:
+class Solution {
+public:
+    int solve(TreeNode* root, int targetSum)
+    {
+        if(targetSum == 0 && !root->left && !root->right)
+            return 1;
+
+        if(root->left)
+            if(solve(root->left, targetSum - root->left->val))
+                return 1;
+        
+        if(root->right)
+            if(solve(root->right, targetSum - root->right->val))
+                return 1;
+
+        return 0;
+    }
+
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(!root)
+            return 0;
+
+        if(solve(root, targetSum-root->val))
+            return 1;
+    
+        return  0;
+    }
+};
+
+//---------------------------------------------------------------
+
+// Method 2:
 class Solution {
 public:
     bool solve(TreeNode* root, int targetSum)
@@ -27,3 +49,16 @@ public:
         return solve(root, targetSum);
     }
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
