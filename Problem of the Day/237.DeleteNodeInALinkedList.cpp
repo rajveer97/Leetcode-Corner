9bf610345e96a8fbb795d142b9dbb47f4c1442ff
Rajveer Pandey <kram35477@gmail.com>
2022-10-13 23:44:42 +0530

    Date: 13.10.22

---
+++ Problem of the Day/237.DeleteNodeInALinkedList.cpp
@@ -0,0 +1,18 @@
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode* temp = node->next;
+        *node = *temp;
+        delete temp;
+    }
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
