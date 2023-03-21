5fe843211bcc046bfeaa861b534fb003faf43dd5
Rajveer Pandey <kram35477@gmail.com>
2023-03-21 23:31:07 +0530

    Date: 21.03.2023

---
+++ Problem of the Day/2348.NumberOfZero-FilledSubarrays.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        long long ans = 0, cnt = 0;
+
+        for(auto x : nums) {
+            if(x)
+                cnt = 0;
+            else 
+                cnt++;
+            
+            ans += cnt;
+        }
+
+        return ans;
+    }
+};
